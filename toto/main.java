e4b1bc0a65ca441edd4676d3ebc490887e675689
Theo Pierson <thheopierson@gmail.com>
2023-07-10 11:25:22 +0200

    feat: add a loop to browse a resultset

--- toto/main.java
+++ toto/main.java
@@ -2,12 +2,18 @@ package toto;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class main {
 
 	public static void main(String[] args) {
 		Connection conn = null;
+		
+
+		Statement stmt = null;
+		ResultSet rs = null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -19,6 +25,15 @@ public class main {
 
 		    conn = DriverManager.getConnection(connectionString);
 		    Article Kebab = new Article(1,"kebab", 6);
+		    
+		    stmt = conn.createStatement();
+		    rs = stmt.executeQuery("SELECT * FROM Article");
+		    while(rs.next()) {
+		    	 System.out.println(rs.getInt(1));
+		    	 System.out.println(rs.getString(2));
+		    	 System.out.println(rs.getInt(3));
+		    }
+		   
 		    
 		} catch (SQLException ex) {
 		    // handle any errors
